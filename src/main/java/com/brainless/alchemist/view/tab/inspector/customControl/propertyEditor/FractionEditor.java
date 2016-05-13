b8d6c05c81d4637d1d13db2c47584c0ba5606cfc
mario meltzow <dev@meltzow.net>
2016-05-13 17:23:12 +0200

    fix nullPointer if Fraction is null

--- src/main/java/com/brainless/alchemist/view/tab/inspector/customControl/propertyEditor/FractionEditor.java
+++ src/main/java/com/brainless/alchemist/view/tab/inspector/customControl/propertyEditor/FractionEditor.java
@@ -14,7 +14,7 @@ import util.math.Fraction;
 public class FractionEditor extends PropertyEditor{
 	TextField valueField;
 	Slider slider;
-	
+
 	public FractionEditor(EntityComponent comp, PropertyDescriptor pd, Consumer3<EntityComponent, String, Object> updateCompFunction) {
 		super(comp, pd, updateCompFunction);
 	}
@@ -23,13 +23,13 @@ public class FractionEditor extends PropertyEditor{
 	protected void createEditor() {
 		HBox box = new HBox(5);
 		setCenter(box);
-		
+
 		valueField = new TextField();
 		valueField.setPrefWidth(100);
 		valueField.addEventHandler(ActionEvent.ACTION, e -> applyChange(e));
 		valueField.focusedProperty().addListener(e -> setEditionMode());
 		box.getChildren().add(valueField);
-		
+
 		slider = new Slider();
 		slider.setMax(1);
 		slider.setMin(0);
@@ -46,15 +46,17 @@ public class FractionEditor extends PropertyEditor{
 	@Override
 	protected void setPropertyValue(Object o) {
 		Fraction a = (Fraction)o;
-		valueField.setText(df.format(a.getValue()));
-		slider.setValue(a.getValue());
+		if (a != null) {
+			valueField.setText(df.format(a.getValue()));
+			slider.setValue(a.getValue());
+		}
 	}
-	
+
 	@Override
 	protected void applyChange(ActionEvent event) {
-		if(event.getSource() == slider)
+		if(event.getSource() == slider) {
 			valueField.setText(df.format(slider.getValue()));
-		else{
+		} else{
 			slider.setValue(Double.parseDouble(valueField.getText()));
 		}
 		super.applyChange(event);
